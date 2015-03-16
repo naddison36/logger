b0003a8f81d78fc8b1bde1432e5b1b71f2b3583b
Nick Addison <nick@addisonbrown.com.au>
2015-03-16 12:49:17 +1100

    initial commit

---
+++ README.md
@@ -0,0 +1,57 @@
+A configurable Winston logger
+===============
+
+A simple wrapper to the [Winston](https://github.com/winstonjs/winston) logger that allows the log level and optional [Loggly](https://www.loggly.com/) configuration to be done in a configuration file using [node-config](https://github.com/lorenwest/node-config)
+
+## Install
+
+This is not currently publish in npm so needs to be installed from the git repo
+`npm install https://github.com/naddison36/logger.git`
+
+## Usage
+
+```javascript
+var logger = require('logger');
+
+// log the stack of an Error object
+var err = new Error('something really bad happened');
+logger.error(err.stack);
+
+// two different logger calls to achieve the same result
+logger.info('some interesting stuff just happened');
+logger.level('info', 'exactly the same stuff just called a different way');
+```
+
+See [Winston doco](https://github.com/winstonjs/winston#logging) for more info
+
+## Config
+Create a logger config file in your config directory in either .yml, .yaml, .coffee, .cson, .properties, .json, .json5, .hjson or .js formats.
+Note the config file needs to be in your project's home folder and not in the logger module
+
+For example, a logger.yaml config file would look like
+```yaml
+---
+  # either trace, debug, info, warn or error
+  logLevel: "info"
+
+  loggly:
+    enable: true
+    subdomain: 'yourLogglySubDomain'
+    inputToken: 'yourLogglyToken'
+    noLogglyOnOsPlatform: 'darwin'
+```
+
+See the config's [Configuration Files](https://github.com/lorenwest/node-config/wiki/Configuration-Files) doco for more information
+
+### Environment instances
+The NODE_APP_INSTANCE environment variable allows for different versions of your logger to run in different environments.
+For example, setting NODE_APP_INSTANCE to prod will look for a logger-prod.EXT config file while
+setting NODE_APP_INSTANCE to test will look for a logger-test.EXT file
+
+If NODE_APP_INSTANCE is not set a logger.EXT file will be loaded
+
+### NODE_CONFIG
+The configuration in the config file can be overridden with the NODE_CONFIG environment variable. eg the following will override the logLevel in the config file to debug
+`NODE_CONFIG='{"logLevel":"debug"}'`
+
+see config's [Environment Variable](https://github.com/lorenwest/node-config/wiki/Environment-Variables) doco for more info
---
+++ index.js
@@ -0,0 +1,85 @@
+var winston = require('winston'),
+    Loggly = require('winston-loggly').Loggly,
+    os = require('os'),
+    config = require('config');
+
+var original_NODE_ENV = process.env.NODE_ENV;
+
+// set environment variable so config looks for the logger config file in the config folder. eg ./config/logger.EXT
+// as per the following config doco, the config file can be in a number of different formats.
+// eg .yml, .yaml, .coffee, .cson, .properties, .json, .json5, .hjson or .js
+// https://github.com/lorenwest/node-config/wiki/Configuration-Files#file-load-order
+
+// environment variable NODE_ENV sets the {deployment} name
+// environment variable NODE_APP_INSTANCE sets the instance name.
+// eg NODE_APP_INSTANCE=test will look for config file ./config/logger-test.EXT
+
+// config items in env var NODE_CONFIG will override anything in the logger config file.
+// eg NODE_CONFIG='{"logLevel":"debug"}' will override the logLevel in the logger config file
+process.env.NODE_ENV = 'logger';
+
+// loads the configuration from the config file
+var loggerConfig = config.util.loadFileConfigs();
+
+// restore original NODE_ENV environment variable
+process.env.NODE_ENV = original_NODE_ENV;
+
+// default the log level if it's not set in the config file or NODE_CONFIG environment variable. eg NODE_CONFIG='{"logLevel":"debug"}'
+var logLevel = loggerConfig.logLevel || "warn";
+
+var logLevels = getLogLevels(loggerConfig);
+
+// instantiate a winston logger with console output
+var logger = module.exports = new (winston.Logger)({
+    levels: logLevels,
+    transports: [
+      new (winston.transports.Console)({level: logLevel, colorize: true})
+    ]
+});
+
+addLoggly(logLevel);
+
+function getLogLevels(loggerConfig)
+{
+    // set custom level colors if in config
+    if (loggerConfig.colors)
+    {
+        winston.addColors(loggerConfig.colors);
+    }
+
+    // if custom levels in config
+    if (loggerConfig.levels)
+    {
+        // return custom levels from config
+        return loggerConfig.levels;
+    }
+    else
+    {
+        // just return the default winston levels
+        return winston.levels;
+
+    }
+}
+
+function addLoggly(logLevel)
+{
+    // if no Loggly configuration or not enable then return
+    if (!loggerConfig.loggly || !loggerConfig.loggly.enable) return;
+
+    // if the process is running on a operating system platform configured in loggly.noLogglyOnOsPlatform then don't add loggly to config
+    // This allows local processes not to use loggly. eg processing running on Mac OSX have a platform name of "darwin"
+    if (os.platform() !== loggerConfig.loggly.noLogglyOnOsPlatform )
+    {
+        // configure Loggly settings
+        var logglyOptions = {
+            level: logLevel,
+            subdomain: loggerConfig.loggly.subdomain,
+            inputToken: loggerConfig.loggly.inputToken,
+            json: false,
+            tag: loggerConfig.loggly.tag || 'logger'
+        };
+
+        // add Loggly to logger
+        logger.add(Loggly, logglyOptions);
+    }
+}
